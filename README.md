2290b79391d68992f8b957927bc12fa82839a4e5
pigram86 <todd@toddpigram.com>
2014-04-01 08:29:03 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# xd7.5 cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Todd Pigram (<todd@toddpigram.com>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'xd7.5'
+maintainer       'Todd Pigram'
+maintainer_email 'todd@toddpigram.com'
+license          'Apache 2.0'
+description      'Installs/Configures xd7.5'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
